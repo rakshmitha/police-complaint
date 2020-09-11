20ff77ab41f98ca1f3757c2e9813a761950d8edc
rakshmitha <rakshmitha17@gmail.com>
2020-09-11 20:29:28 +0530

    registration page added

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect,session
+Import os
 
 app=Flask(__name__)
+app.secret_key=os.urandom(24)
+
+conn=
+cursor=conn.cursor()
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    if 'user_id' in session:
+        return render_template('login.html')
+    else:
+        return redirect('/')
 
 @app.route('/register')
 def about():
@@ -18,7 +26,29 @@ def dashboard():
 def login_validation():
     email=request.form.get('email')
     password=request.form.get('password')
-    return "the email is {} and the pass is {}".format(email,password)
+
+    cursor.execute("""SELECT * FROM `users` WHERE `email` LIKE `{}` AND `password` LIKE `{}`""" .format(email, password))
+    users=cursor.fetchall()
+    if len(users)>0:
+        session['user_id']=users[0][0]
+        return redirect('dashboard.html')
+    else:
+        return redirect('/')
+
+@app.route('/add_user', methods=['POST','GET'])
+def add_user():
+    name=request.form.get('uname')
+    email=request.form.get('uemail')
+    password=request.form.get('upassword')
+
+    cursor.execute("""INSERT INTO `users` (`user_id,`name`,`email`,`password`) VALUES (NULL, '{}', '{}', '{}')""".format(name,email,password))
+    conn.commit()
+    return "User Registered Successfully"
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id')
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
