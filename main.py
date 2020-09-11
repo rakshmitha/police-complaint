0383893cb8bc80d629575b772428f9fc69d515ab
rakshmitha <rakshmitha17@gmail.com>
2020-09-11 19:24:25 +0530

    home page

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/register')
+def about():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
